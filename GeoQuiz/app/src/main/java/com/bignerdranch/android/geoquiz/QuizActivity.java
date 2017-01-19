03d76775323351245205a41b961bab84678da1da
leomc <leomc@naver.com>
2017-01-19 20:36:15 +0900

    QuizActivity에서 장치를 회전시켜 mIsCheater값을 지우는 경우를 방지.

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -15,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String CLEVER_CHEATER = "cheater";
     private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
@@ -124,6 +125,7 @@ public class QuizActivity extends AppCompatActivity {
         });
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(CLEVER_CHEATER, false);
         }
 
         updateQuestion();
@@ -148,6 +150,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(CLEVER_CHEATER, mIsCheater);
     }
 
     @Override
