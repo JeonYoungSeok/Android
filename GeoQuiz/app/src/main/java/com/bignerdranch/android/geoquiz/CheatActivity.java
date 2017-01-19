1580c75d3887c8e72e8890fe363f221997542d10
leomc <leomc@naver.com>
2017-01-19 20:16:32 +0900

    CheatActivity에서 화면 회전했을 때 정보전달을 통해 자료의 손실을 막음.

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,8 +13,11 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.getquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.getquiz.answer_shown";
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_INDEX = "index";
 
     private boolean mAnswerIsTrue;
+    private boolean mAnswerIsShown;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
 
@@ -33,6 +37,8 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
+        mAnswerIsShown = false;
+
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
@@ -45,13 +51,33 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                mAnswerIsShown = true;
             }
         });
+
+        if (savedInstanceState != null) {
+            mAnswerIsShown = savedInstanceState.getBoolean(KEY_INDEX, false);
+            setAnswerShownResult(mAnswerIsShown);
+            if(mAnswerIsShown) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        }
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_INDEX, mAnswerIsShown);
     }
 }
