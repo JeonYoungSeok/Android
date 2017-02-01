1a993d6cc8fa28d5ac5888daaaa361e40e9336a3
leomc <leomc@naver.com>
2017-02-01 21:54:16 +0900

    버튼 애니메이션 추가 및 챌린지(빌드 버전 보여주기)

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +24,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsShown;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private TextView mVersionView;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent i = new Intent(packageContext, CheatActivity.class);
@@ -52,8 +57,28 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 setAnswerShownResult(true);
                 mAnswerIsShown = true;
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        mVersionView = (TextView) findViewById(R.id.version_view);
+        mVersionView.setText("API 레벨 " + Build.VERSION.SDK_INT);
 
         if (savedInstanceState != null) {
             mAnswerIsShown = savedInstanceState.getBoolean(KEY_INDEX, false);
